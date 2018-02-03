7d917c47ecd7eb3a871eeacb3457f72e982a6af2
itamar.curiel <itamar.curiel@tufin.com>
2018-02-03 21:48:39 +0200

    all files should be here

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import numpy as np
-from typing import List, Tuple, Dict
-import random
-
-
 class Data:
-    def __init__(self):
-        pass
-
-    def init_from_file(self, file_name: str):
+    def __init__(self, file_name: str):
         with open(file_name) as file:
             self.num_rows, self.num_columns, self.num_drones, \
             self.num_turns, self.max_payload = map(int, file.readline().split(" "))
@@ -17,8 +9,7 @@ class Data:
 
 
 def main():
-    data = Data()
-    data.init_from_file("busy_day.in")
+    data = Data("busy_day.in")
 
 
 if __name__ == '__main__':
